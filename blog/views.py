773b1527a35dc86298940bd9934e3914dd46e9b4
Ilya Osipov <070809010@mail.ru>
2017-09-19 19:58:30 +0300

    Fix bug where everybody could choose author

--- blog/views.py
+++ blog/views.py
@@ -72,6 +72,7 @@ def add_comment_to_post(request, pk):
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
+            comment.author = request.user
             comment.save()
             return redirect('post_detail', pk=post.pk)
     else:
