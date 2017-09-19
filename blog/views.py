7213365bcfc4af6d01aca0375372bda0a2bc29e2
Ilya Osipov <070809010@mail.ru>
2017-09-19 16:35:28 +0300

    Forbid some views for non-superusers

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 from django.utils import timezone
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 def post_list(request):
@@ -20,7 +22,7 @@ def post_detail(request, pk):
         return render(request, 'blog/post_detail.html', {'post': post})
 
 
-@login_required
+@staff_member_required
 def post_new(request):
         if request.method == "POST":
             form = PostForm(request.POST)
@@ -34,7 +36,7 @@ def post_new(request):
         return render(request, 'blog/post_edit.html', {'form': form})
 
 
-@login_required
+@staff_member_required
 def post_edit(request, pk):
 		post = get_object_or_404(Post, pk=pk)
 		if request.method == "POST":
@@ -49,14 +51,14 @@ def post_edit(request, pk):
 		return render(request, 'blog/post_edit.html', {'form': form})
 
 
-@login_required
+@staff_member_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
 
-@login_required
+@staff_member_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
