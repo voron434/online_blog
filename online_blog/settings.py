cffe4e8d1018d4c655e09fbcd0e37ae16090311b
Ilya Osipov <070809010@mail.ru>
2017-09-19 16:36:19 +0300

    Refactor project settings for new app

--- online_blog/settings.py
+++ online_blog/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'blog'
+    'blog',
+    'registration',
 ]
 
 MIDDLEWARE = [
--- online_blog/urls.py
+++ online_blog/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
-from django.contrib.auth import views
 from django.contrib import admin
+from django.contrib.auth import views
+
 
 urlpatterns = [
-    url(r'', include('blog.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'', include('blog.urls')),
+	url(r'', include('registration.urls')),
 ]
