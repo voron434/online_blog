f297b2ae004dcb1581b462e50ab35d1f025396a8
Ilya Osipov <070809010@mail.ru>
2017-09-23 01:34:45 +0300

    Fix approved_comments method

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+        
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
 
 class Comment(models.Model):
@@ -32,6 +35,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-def approved_comments(self):
-    return self.comments.filter(approved_comment=True)
